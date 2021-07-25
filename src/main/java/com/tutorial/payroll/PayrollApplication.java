1718d05c9bb5aa2ab1b85e0f968e79eea17c2e3e
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2021-07-25 22:53:26 +0800

    Added CommandLine runner and repository

--- src/main/java/com/tutorial/payroll/PayrollApplication.java
+++ src/main/java/com/tutorial/payroll/PayrollApplication.java
@@ -3,6 +3,14 @@ package com.tutorial.payroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/*
+ @SpringBootApplication
+ meta annotation that pulls the following:
+ 1. component scanning
+ 2. autoconfiguration
+ 3. property support
+ */
 @SpringBootApplication
 public class PayrollApplication {
 
